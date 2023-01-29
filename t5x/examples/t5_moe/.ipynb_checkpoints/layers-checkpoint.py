196863fa1ff46524558b74ab7943398a43837cea
Xue Fuzhao <xuefuzhao@outlook.com>
2023-01-29 16:31:08 +0000

    moe debug

--- t5x/examples/t5_moe/.ipynb_checkpoints/layers-checkpoint.py
+++ t5x/examples/t5_moe/.ipynb_checkpoints/layers-checkpoint.py
@@ -549,7 +549,7 @@ class MoEEmbed(nn.Module):
 
   def __call__(self, inputs: Array,
                deterministic: bool = False,
-               embed_select_decision: Optional[Array]=None) -> Array:
+               embed_select_decision: Optional[Array] = None) -> Array:
     """Embeds the inputs along the last dimension.
 
     Args:
@@ -572,10 +572,13 @@ class MoEEmbed(nn.Module):
                                                  shape=[inputs.shape[0]],
                                                  min_val=0,
                                                  max_val=self.moe_emb_num)
+      
+      embed_select_decision = with_sharding_constraint(embed_select_decision, ('batch'))
       embed_select_decision = nn.one_hot(embed_select_decision, self.moe_emb_num)
       print(embed_select_decision.shape)
       iota = lax.iota(jnp.int32, self.num_embeddings)
       one_hot = jnp.array(inputs[..., jnp.newaxis] == iota, dtype=self.dtype)
+      
       output = jnp.dot(one_hot[jnp.newaxis, ...], jnp.asarray(self.embedding, self.dtype))
       output = jnp.dot(embed_select_decision.T[..., jnp.newaxis, jnp.newaxis], output)
       
