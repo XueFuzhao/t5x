6da0a7564f39befc3f8130ec90160238e77d8115
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-02-06 18:52:54 +0800

    Update utils.py

--- t5x/utils.py
+++ t5x/utils.py
@@ -393,11 +393,11 @@ class LegacyCheckpointManager(orbax.checkpoint.CheckpointManager):
     for path in paths:
       logging.info('Initializing parameters from specific T5X checkpoint %s',
                    path)
-      print(path)
-      if path == 'target/token_embedder/embedding':
-          logging.info('Skip parameters from T5X checkpoint %s',
-                   path)
-          continue
+#       print(path)
+#       if path == 'target/token_embedder/embedding':
+#           logging.info('Skip parameters from T5X checkpoint %s',
+#                    path)
+#           continue
       restored.append(
           self._checkpointer.restore(
               path=path,
