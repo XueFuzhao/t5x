e307717b9a6a5e54a5c61338ab8405a50c241510
Xue Fuzhao <57164838+XueFuzhao@users.noreply.github.com>
2023-02-07 16:04:34 +0800

    Update layers.py

--- t5x/examples/t5_moe/layers.py
+++ t5x/examples/t5_moe/layers.py
@@ -564,31 +564,42 @@ class MoEEmbed(nn.Module):
       inputs = inputs.astype(self.cast_input_dtype)
     if not jnp.issubdtype(inputs.dtype, jnp.integer):
       raise ValueError('Input type must be an integer or unsigned integer.')
-    '''
+    
     # Using the first MoE during inference.
     if self.one_hot:
-      if embed_select_decision is None:
+      if embed_select_decision_1 is None or embed_select_decision_2 is None:
         if deterministic:
-          embed_select_decision = jnp.zeros([inputs.shape[0]], self.dtype)
+          embed_select_decision_1 = jnp.zeros([inputs.shape[0]], self.dtype)
+          embed_select_decision_2 = jnp.ones([inputs.shape[0]], self.dtype)
         else:
           routing_rng = self.make_rng('dropout')
-          embed_select_decision = jax.random.randint(key=routing_rng,
-                                                 shape=[inputs.shape[0]],
-                                                 minval=0,
-                                                 maxval=self.moe_emb_num)
-        print(embed_select_decision.shape)
-        embed_select_decision = with_sharding_constraint(embed_select_decision, ('batch',))
+          embed_select_decision = jax.random.choice(routing_rng,
+                            jnp.arange(0,self.moe_emb_num),
+                            shape=[2, inputs.shape[0]])
+          embed_select_decision_1 = embed_select_decision[0,:]
+          embed_select_decision_2 = embed_select_decision[1,:]
+                            
+#           embed_select_decision_1 = jax.random.randint(key=routing_rng,
+#                                                  shape=[inputs.shape[0]],
+#                                                  minval=0,
+#                                                  maxval=self.moe_emb_num)
+        embed_select_decision_1 = with_sharding_constraint(embed_select_decision_1, ('batch',))
+        embed_select_decision_2 = with_sharding_constraint(embed_select_decision_2, ('batch',))
         moe_iota = lax.iota(jnp.int32, self.moe_emb_num)
-        embed_select_decision = jnp.array(embed_select_decision[..., jnp.newaxis] == moe_iota, dtype=self.dtype)
-      embed_select_decision = with_sharding_constraint(embed_select_decision, ('batch', 'moe_embed'))
+        embed_select_decision_1 = jnp.array(embed_select_decision_1[..., jnp.newaxis] == moe_iota, dtype=self.dtype)
+        embed_select_decision_2 = jnp.array(embed_select_decision_2[..., jnp.newaxis] == moe_iota, dtype=self.dtype)
+      embed_select_decision_1 = with_sharding_constraint(embed_select_decision_1, ('batch', 'moe_embed'))
+      embed_select_decision_2 = with_sharding_constraint(embed_select_decision_2, ('batch', 'moe_embed'))
       iota = lax.iota(jnp.int32, self.num_embeddings)
       one_hot = jnp.array(inputs[..., jnp.newaxis] == iota, dtype=self.dtype)
       
       output = jnp.dot(one_hot, jnp.asarray(self.embedding, self.dtype))
-      output = jnp.einsum('blme,bm->ble', output, embed_select_decision)
-      output = with_sharding_constraint(output, ('batch', 'length', 'embed'))'''
+      output_1 = jnp.einsum('blme,bm->ble', output, embed_select_decision_1)
+      output_2 = jnp.einsum('blme,bm->ble', output, embed_select_decision_2)
+      output = output_1 - output_2 
+      output = with_sharding_constraint(output, ('batch', 'length', 'embed'))
 
-    # Mering MoE during infernce.
+    '''# Mering MoE during infernce.
     if self.one_hot:
       if deterministic:
           # This embed_select_decision will not be really used during inference.
@@ -616,7 +627,7 @@ class MoEEmbed(nn.Module):
 
           output = jnp.dot(one_hot, jnp.asarray(self.embedding, self.dtype))
           output = jnp.einsum('blme,bm->ble', output, embed_select_decision)
-          output = with_sharding_constraint(output, ('batch', 'length', 'embed'))
+          output = with_sharding_constraint(output, ('batch', 'length', 'embed'))'''
       
     else:
       output = jnp.asarray(self.embedding, self.dtype)[inputs]
